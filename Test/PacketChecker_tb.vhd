73edc393e20d14c4bca51edcdb0179bfa28c26de
Dehghani <dehghanim2002@gmail.com>
2024-04-02 01:00:36 +0330

    first tb added

--- Test/PacketChecker_tb.vhd
+++ Test/PacketChecker_tb.vhd
@@ -2,29 +2,30 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity UART_tb is
-end UART_tb;
+entity PacketChecker_tb is
+end PacketChecker_tb;
 
-architecture behavioral of UART_tb is
-    component UART is
+architecture behavioral of PacketChecker_tb is
+    component PacketChecker is
         generic (
-            parity : string := "Even"; -- "None" , "Even" , "Odd"
-            stopBitsNum : integer := 1;
-            clkFreq : integer := 100_000_000;
-            baudRate : integer := 19200
+            header : std_logic_vector(8 - 1 downto 0) := x"3B"
         );
         port (
             clk : in std_logic;
             rst : in std_logic;
 
-            Rx : in std_logic;
-            Tx : out std_logic;
-
-            dataIn : in std_logic_vector(8 - 1 downto 0);
-            dataInRdy : in std_logic;
-
-            dataOut : out std_logic_vector(8 - 1 downto 0);
+            dataIn     : in std_logic_vector(8 - 1 downto 0);
+            dataInRdy  : in std_logic;
+            dataOut    : out std_logic_vector(8 - 1 downto 0);
             dataOutRdy : out std_logic;
+
+            wrAddress : out std_logic_vector(16 - 1 downto 0);
+            wrData    : out std_logic_vector(32 - 1 downto 0);
+            wrEn      : out std_logic;
+            rdAddress : out std_logic_vector(16 - 1 downto 0);
+            rdData    : in std_logic_vector(32 - 1 downto 0);
+            rdRdy     : in std_logic;
+            rdEn      : out std_logic;
 
             error : out std_logic
         );
@@ -32,48 +33,30 @@ architecture behavioral of UART_tb is
 
     constant periodCLK : time := 20ns;
 
-    signal clk : std_logic;
-    signal rst : std_logic := '0';
-
-    signal Rx : std_logic := '1';
-    signal Tx : std_logic;
-
-    signal dataIn : std_logic_vector(8 - 1 downto 0);
+    signal clk       : std_logic;
+    signal rst       : std_logic := '0';
+    signal dataIn    : std_logic_vector(8 - 1 downto 0);
     signal dataInRdy : std_logic := '0';
 
-    signal dataOut : std_logic_vector(8 - 1 downto 0);
+    signal dataOut    : std_logic_vector(8 - 1 downto 0);
     signal dataOutRdy : std_logic;
+
+    signal wrAddress : std_logic_vector(16 - 1 downto 0);
+    signal wrData    : std_logic_vector(32 - 1 downto 0);
+    signal wrEn      : std_logic;
+    signal rdAddress : std_logic_vector(16 - 1 downto 0);
+    signal rdData    : std_logic_vector(32 - 1 downto 0);
+    signal rdRdy     : std_logic;
+    signal rdEn      : std_logic;
 
     signal error : std_logic;
 begin
 
-    UARTInst : UART port map
-        (clk, rst, Rx, Tx, dataIn, dataInRdy, dataOut, dataOutRdy, error);
+    PacketCheckerInst : PacketChecker port map
+        (clk, rst, dataIn, dataInRdy, dataOut, dataOutRdy, wrAddres, wrData, wrEn, rdAddress, rdData, rdRdy, rdEn, error);
 
     process
     begin
-        for i in 1 to 10 loop
-            clk <= '1';
-            wait for periodCLK/2;
-            clk <= '0';
-            wait for periodCLK/2;
-        end loop;
-        dataIn <= "10101010";
-        dataInRdy <= '1';
-        clk <= '1';
-        wait for periodCLK/2;
-        clk <= '0';
-        wait for periodCLK/2;
-        dataInRdy <= '0';
-        for i in 1 to 10 loop
-            clk <= '1';
-            wait for periodCLK/2;
-            clk <= '0';
-            wait for periodCLK/2;
-        end loop;
-
-        dataInRdy <= '0';
-        dataIn <= "00000000";
         for i in 1 to 10 loop
             clk <= '1';
             wait for periodCLK/2;
